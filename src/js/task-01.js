143b4201cbea659e6e9e8d2915ee9c7bb805bdd8
Oksana Semko <osemko@gmail.com>
2022-02-19 02:12:22 +0200

    Add solutions to task-02 & task-03

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -6,3 +6,11 @@ subCategoryNumEl.forEach((category) => {
   console.log("Category: ", category.firstElementChild.textContent);
   console.log("Element: ", category.lastElementChild.children.length);
 });
+
+const categoriesEl = document.querySelectorAll(".item");
+console.log("Number of categories ", categoriesEl.length);
+
+categoriesEl.forEach(function (category) {
+  console.log("Category: ", category.firstElementChild.textContent);
+  console.log("Element: ", category.lastElementChild.children.length);
+});
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const arrayList = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = ingredient;
+  return listItem;
+});
+
+document.querySelector("#ingredients").append(...arrayList);
