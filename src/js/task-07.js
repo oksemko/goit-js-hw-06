ccd3221e4dbf3423a2655858918e9742e356df01
Oksana Semko <osemko@gmail.com>
2022-05-16 21:12:48 +0300

    task_07_solution

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,6 @@
-const inputEl = document.querySelector("font-size-control");
-const spanEl = document.querySelector(#text);
+const inputEl = document.querySelector("#font-size-control");
+const spanEl = document.querySelector("#text");
 
-inputEl.addEventListener(
-    "input", (event) => (spanEl.style.fontSize = `${event.currentTarget.value}px`)
-);
+inputEl.addEventListener("input", (event) => {
+  spanEl.style.fontSize = `${event.currentTarget.value}px`;
+});
